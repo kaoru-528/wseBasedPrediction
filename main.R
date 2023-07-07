145aa01b865d48e6ed8cc9eb8712aaab5d853cfb
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2023-07-07 13:03:53 +0900

    some fix

--- main.R
+++ main.R
@@ -1,3 +1,16 @@
 # Load data set
 dataPath = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/DS/NDT_WSE/16-58-32_var_J=3.RData")
 load(dataPath)
+Cs = hard$Cs
+Ds = hard$Ds
+dDs = hard$Denoise_Ds
+
+tmp_Cs = list()
+tmp_Ds = list()
+for(i in seq(1, 62, by=8)){
+    print(i)
+    tmp_Cs = c(tmp_Cs,Cs[i])
+    tmp_Ds = c(tmp_Ds,Ds[i])
+}
+
+plot(x = c(1:i),y = tmp_Cs[[1]][[1]])
