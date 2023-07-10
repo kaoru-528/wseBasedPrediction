ba8b10a46792a1b757029e2428dae8ac34ea48f7
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2023-07-10 22:05:46 +0900

    some fix

--- main.R
+++ main.R
@@ -6,11 +6,18 @@ Ds = hard$Ds
 dDs = hard$Denoise_Ds
 
 tmp_Cs = list()
+tmp_Cs_1 = list()
+tmp_Ds_1 = list()
 tmp_Ds = list()
+j = 1
 for(i in seq(1, 62, by=8)){
-    print(i)
     tmp_Cs = c(tmp_Cs,Cs[i])
+    tmp_Cs_1 = c(tmp_Cs_1,tmp_Cs[[j]][[4]])
     tmp_Ds = c(tmp_Ds,Ds[i])
+    tmp_Ds_1 = c(tmp_Ds_1,tmp_Ds[[j]][[4]])
+
+    j = j + 1 
 }
 
-plot(x = c(1:i),y = tmp_Cs[[1]][[1]])
+plot(c(1:length(tmp_Cs_1)), tmp_Cs_1,main = "C[1][4]", xlab = "number", ylab = "C_coe")
+plot(c(1:length(tmp_Ds_1)), tmp_Ds_1)
